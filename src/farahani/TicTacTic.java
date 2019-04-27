321c1851745e9bceb4e7ca5349434ae8f2411b7f
MfDevNet <MfDevNet@gmail.com>
2019-04-27 20:38:19 +0430

    add constructor

--- src/farahani/TicTacTic.java
+++ src/farahani/TicTacTic.java
@@ -1,5 +1,27 @@
 package farahani;
 
-public class TicTacTic {
+import javax.swing.*;
 
+public class TicTacTic extends JFrame {
+    static Boolean result;
+    static int countX;
+    static int countO;
+    static boolean flag;
+    JButton[] jbArray;
+    JButton[] jbArrayResult;
+
+    /**
+     * Creates new form TicTacTic
+     */
+    public TicTacTic() {
+        setfuture();
+
+        initComponents();
+        this.jbArray = new JButton[]{jButton1, jButton2, jButton3, jButton4, jButton5, jButton6, jButton7, jButton8,
+                jButton9};
+        this.jbArrayResult = new JButton[]{jButton12, jButton13, jButton14, jButton15, jButton16, jButton17,
+                jButton18, jButton19, jButton20};
+
+        resetGame();
+    }
 }
