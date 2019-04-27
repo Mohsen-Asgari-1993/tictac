8eee20c413cd19abcb2721db6b6fed738720fadc
MfDevNet <MfDevNet@gmail.com>
2019-04-27 20:42:13 +0430

    fix Game

--- src/farahani/TicTacTic.java
+++ src/farahani/TicTacTic.java
@@ -14,17 +14,7 @@ public class TicTacTic extends JFrame {
     /**
      * Creates new form TicTacTic
      */
-    public TicTacTic() {
-        setfuture();
 
-        initComponents();
-        this.jbArray = new JButton[]{jButton1, jButton2, jButton3, jButton4, jButton5, jButton6, jButton7, jButton8,
-                jButton9};
-        this.jbArrayResult = new JButton[]{jButton12, jButton13, jButton14, jButton15, jButton16, jButton17,
-                jButton18, jButton19, jButton20};
-
-        resetGame();
-    }
 
     /**
      * Creates new form TicTacTic
